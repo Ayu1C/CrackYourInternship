24f16a5ffe4366b745bab3e12db6c16315dcb832
Ayushi Chouhan <ayushichouhan2003@gmail.com>
2023-07-06 20:48:29 +0530

    Time: 148 ms (16.50%), Space: 43.8 MB (50.88%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,6 +2,18 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         
+//         we find out, that a subarray may also exist if this particular condition satisified, i.e
+//     (prefix[i] - k) should exist in our map, 
+	
+// 	How? see,
+	
+// 	If prefix[i] - prefix[j] = k where i > j (i, j any index, in our case they are i = 3, j = 0)
+// 	prefix[i] - k = prefix[j] na, (simple maths)
+	
+// 	i.e if we are able to find (prefix[i] - k) is exist in our map, i.e prefix[j]
+// 	so, we can say that from index j + 1 to i, the sum of the elements is k, right na,
+// 	hence it proves that their exist an subarray from index j + 1 to i whose sum is k.
+        
         int count = 0;
         
         map<int, int> sum;
