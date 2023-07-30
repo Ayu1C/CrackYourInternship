749328014095383fdda0fbae54ef41eb15b29cdf
Ayushi Chouhan <ayushichouhan2003@gmail.com>
2023-07-30 12:37:49 +0530

    Time: 367 ms (36.36%), Space: 10.3 MB (59.20%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -20,6 +20,22 @@ public:
             }
             if(found == 1) break;
         }
+        
         return ans;
     }
 };
+
+//A more optimized approach using hash table (i.e. unordered map)
+
+// vector<int> twoSum(vector<int>& nums, int target) {
+//         unordered_map<int, int> mp;
+       
+//         for(int i = 0; i < nums.size(); i++){
+//             if(mp.find(target - nums[i]) == mp.end())
+//                 mp[nums[i]] = i;
+//             else
+//                 return {mp[target - nums[i]], i};
+//         }
+ 
+//         return {-1, -1};
+// }
