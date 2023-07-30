4a031047cee11365df05b8ae6f6aacede437f8dd
Ayushi Chouhan <ayushichouhan2003@gmail.com>
2023-07-30 12:45:07 +0530

    Time: 7 ms (96.94%), Space: 10.8 MB (44.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,41 +1,49 @@
 class Solution {
 public:
+    
+    //This approach has O(n^2) time complexity.
+    
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//       vector<int> v, ans;
+        
+//         bool found = 0;
+        
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             v.push_back(target - nums[i]);
+//             for(int j=0; j<v.size(); j++)
+//             {
+//                if(v[j]==nums[i] && i!=j)
+//                {
+//                    ans.push_back(i);
+//                    ans.push_back(j);
+//                    found = 1;
+//                    break;
+//                }
+//             }
+//             if(found == 1) break;
+//         }
+        
+//         return ans;
+//     }
+    
+    
+    //A more optimized approach using hash table (i.e. unordered map)
+    
     vector<int> twoSum(vector<int>& nums, int target) {
-      vector<int> v, ans;
-        
-        bool found = 0;
-        
-        for(int i=0; i<nums.size(); i++)
-        {
-            v.push_back(target - nums[i]);
-            for(int j=0; j<v.size(); j++)
-            {
-               if(v[j]==nums[i] && i!=j)
-               {
-                   ans.push_back(i);
-                   ans.push_back(j);
-                   found = 1;
-                   break;
-               }
-            }
-            if(found == 1) break;
+        unordered_map<int, int> mp;
+       
+        for(int i = 0; i < nums.size(); i++){
+            if(mp.find(target - nums[i]) == mp.end())
+                mp[nums[i]] = i;
+            else
+                return {mp[target - nums[i]], i};
         }
-        
-        return ans;
-    }
+ 
+        return {-1, -1};
+}
+
+    
 };
 
-//A more optimized approach using hash table (i.e. unordered map)
 
-// vector<int> twoSum(vector<int>& nums, int target) {
-//         unordered_map<int, int> mp;
-       
-//         for(int i = 0; i < nums.size(); i++){
-//             if(mp.find(target - nums[i]) == mp.end())
-//                 mp[nums[i]] = i;
-//             else
-//                 return {mp[target - nums[i]], i};
-//         }
- 
-//         return {-1, -1};
-// }
