291644d8eff2ffc8bbe31c053bf7a0d740896233
Ayushi Chouhan <ayushichouhan2003@gmail.com>
2023-08-03 19:43:58 +0530

    Time: 15 ms (96.96%), Space: 13.8 MB (53.59%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        vector<int> adj[numCourses];
+        for(auto it : prerequisites)
+        {
+            //direction of edge is such that we have to finish prerequisite first, then the course.
+            //Here, in Ex 1, graph edge will be directed from 0 to 1;
+            //adj[i] stores all nodes to which edges are directed from node i;
+            adj[it[1]].push_back(it[0]);
+        }
+        
+        vector<int> indegree(numCourses, 0);
+        
+        for(int i=0; i<numCourses; i++)
+        {
+            for(auto it: adj[i])
+            {
+                indegree[it]++;
+            }
+        }
+        
+        queue<int> q;
+        
+        for(int i=0; i<numCourses; i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        
+        vector<int> topo;
+        
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            
+            for(auto it : adj[node])
+            {
+               indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+        
+        if(topo.size()==numCourses) return topo;
+        else return {};
+    }
+};
