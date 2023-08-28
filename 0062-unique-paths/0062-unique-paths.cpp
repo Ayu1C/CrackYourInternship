aad9ffdcc6230ec8092d2ac08a2966ce67894aed
Ayushi Chouhan <ayushichouhan2003@gmail.com>
2023-08-29 02:59:22 +0530

    Time: 3 ms (33.76%), Space: 6.3 MB (67.42%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -9,33 +9,40 @@ public:
         
 //         if(row==0 && col==0) return 1;
         
-//         int right = f(row, col-1, dp);
-//         int down = f(row-1, col, dp);
+//         int left = f(row, col-1, dp);
+//         int up = f(row-1, col, dp);
         
-//         return dp[row][col] = right+down;
+//         return dp[row][col] = left+up;
 //     }
 
     int uniquePaths(int m, int n) {
         
         if((m==1 && n==1) || (m==1 && n==2) || (m==2 && n==1)) return 1;
         
-        vector<vector<int>> dp(m, vector<int> (n, 0));
+        // vector<vector<int>> dp(m, vector<int> (n, 0));
         
         //dp[i][j] stores the number of unique paths to reach grid[i][j] from grid[0][0]
          
-        dp[0][0] = 1;
+        
+        vector<int> prev(n,0);
         
         for(int i=0; i<m; i++)
-        {      
+        {   
+            vector<int> temp(n,0);
             for(int j=0; j<n; j++)
             {
-                if(j-1>=0) dp[i][j] += dp[i][j-1];
-                
-                if(i-1>=0) dp[i][j] += dp[i-1][j];
+                if(i==0 && j==0){
+                    temp[0] = 1;
+                    continue;
+                }
+                int cur = prev[j];
+                if(j-1>=0) cur += temp[j-1];
+                temp[j] = cur;
             }
+            prev = temp;
         }
             
-         return dp[m-1][n-1];   
+         return prev[n-1];   
     }
     
 };
