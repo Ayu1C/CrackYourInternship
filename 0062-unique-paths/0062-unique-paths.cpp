fbc07a3a5dc0c71ffc1c97d42f16fdd11686b29a
Ayushi Chouhan <ayushichouhan2003@gmail.com>
2023-08-29 01:53:46 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (53.19%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int f(int row, int col, vector<vector<int>>& dp)
+    {
+        
+        if(row<0 || col<0) return 0;
+        
+        if(dp[row][col] != -1) return dp[row][col];
+        
+        if(row==0 && col==0) return 1;
+        
+        int right = f(row, col-1, dp);
+        int down = f(row-1, col, dp);
+        
+        return dp[row][col] = right+down;
+    }
+
+        
+        
+    int uniquePaths(int m, int n) {
+        
+        if((m==1 && n==1) || (m==1 && n==2) || (m==2 && n==1)) return 1;
+        
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        
+        //dp[i][j] stores the number of unique paths to reach grid[i][j] from grid[0][0]
+         
+        return f(m-1, n-1, dp);
+    }
+};
