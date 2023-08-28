84d439c57637f5d97265c2375e1da73f044e1f7b
Ayushi Chouhan <ayushichouhan2003@gmail.com>
2023-08-29 02:14:03 +0530

    Time: 3 ms (33.76%), Space: 6.5 MB (23.70%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,30 +1,41 @@
 class Solution {
 public:
-    int f(int row, int col, vector<vector<int>>& dp)
-    {
+//     int f(int row, int col, vector<vector<int>>& dp)
+//     {
         
-        if(row<0 || col<0) return 0;
+//         if(row<0 || col<0) return 0;
         
-        if(dp[row][col] != -1) return dp[row][col];
+//         if(dp[row][col] != -1) return dp[row][col];
         
-        if(row==0 && col==0) return 1;
+//         if(row==0 && col==0) return 1;
         
-        int right = f(row, col-1, dp);
-        int down = f(row-1, col, dp);
+//         int right = f(row, col-1, dp);
+//         int down = f(row-1, col, dp);
         
-        return dp[row][col] = right+down;
-    }
+//         return dp[row][col] = right+down;
+//     }
 
-        
-        
     int uniquePaths(int m, int n) {
         
         if((m==1 && n==1) || (m==1 && n==2) || (m==2 && n==1)) return 1;
         
-        vector<vector<int>> dp(m, vector<int> (n, -1));
+        vector<vector<int>> dp(m, vector<int> (n, 0));
         
         //dp[i][j] stores the number of unique paths to reach grid[i][j] from grid[0][0]
          
-        return f(m-1, n-1, dp);
+        dp[0][0] = 1;
+        
+        for(int i=0; i<m; i++)
+        {      
+            for(int j=0; j<n; j++)
+            {
+                if(j-1>=0) dp[i][j] += dp[i][j-1];
+                
+                if(i-1>=0) dp[i][j] += dp[i-1][j];
+            }
+        }
+            
+         return dp[m-1][n-1];   
     }
+    
 };
