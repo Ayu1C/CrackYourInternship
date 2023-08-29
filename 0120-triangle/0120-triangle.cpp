3135aa13e14fa4316eff151d17dec0f903b9456c
Ayushi Chouhan <ayushichouhan2003@gmail.com>
2023-08-30 01:17:14 +0530

    Time: 4 ms (81.27%), Space: 9 MB (22.44%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,33 +1,57 @@
 class Solution {
 public:
     
-    int f(int row, int col, vector<vector<int>>& triangle, int n, vector<vector<int>>& dp)
-    {  
-        if(dp[row][col] != 1e4+1) return dp[row][col];
+    //USING MEMOIZATION
+    
+//     int f(int row, int col, vector<vector<int>>& triangle, int n, vector<vector<int>>& dp)
+//     {  
+//         if(dp[row][col] != 1e4+1) return dp[row][col];
         
-        if(row==n-1) return triangle[row][col];
+//         if(row==n-1) return triangle[row][col];
         
-        int idx1 = triangle[row][col] + f(row+1, col, triangle, n, dp) ;
-        int idx2 = triangle[row][col] + f(row+1, col+1, triangle, n, dp);
+//         int idx1 = triangle[row][col] + f(row+1, col, triangle, n, dp) ;
+//         int idx2 = triangle[row][col] + f(row+1, col+1, triangle, n, dp);
         
-        return dp[row][col] = min(idx1, idx2);
-    }
+//         return dp[row][col] = min(idx1, idx2);
+//     }
     
     
-    int minimumTotal(vector<vector<int>>& triangle) {
+//     int minimumTotal(vector<vector<int>>& triangle) {
        
+//         int n = triangle.size();
+
+//         vector<vector<int>> dp(n, vector<int>(n, 1e4+1));
+        
+//         for(int i=0; i<n; i++)
+//         {
+//             dp[i].resize(i+1);
+//         }
+        
+//         return f(0, 0, triangle, n, dp);
+//     }
+    
+    //USING TABULATION 
+    
+    int minimumTotal(vector<vector<int>>& triangle){
+        
         int n = triangle.size();
 
-        vector<vector<int>> dp(n, vector<int>(n, 1e4+1));
+        vector<vector<int>> dp(n, vector<int>(n, 0));
         
         for(int i=0; i<n; i++)
         {
-            // vector<int> temp(i+1, 1e4+1);
-            // dp.push_back(temp);
-            
-            dp[i].resize(i+1);
+            dp[n-1][i] = triangle[n-1][i];
         }
         
-        return f(0, 0, triangle, n, dp);
+        for(int i=n-2; i>=0; i--)
+        {
+            for(int j=i; j>=0; j--)
+            {
+                dp[i][j] = min(dp[i+1][j] + triangle[i][j], dp[i+1][j+1] + triangle[i][j]);
+            }
+        }
+        
+        return dp[0][0];
     }
+    
 };
