972b073656f99abf9f50aa98915542d0ed803967
Ayushi Chouhan <ayushichouhan2003@gmail.com>
2023-09-16 04:17:48 +0530

    Time: 36 ms (27.14%), Space: 18.5 MB (41.52%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
 
-    int f(vector<int>& arr, int ind, int T, vector<vector<int>>& dp){
- 
-           if(ind == 0){
-               return (T%arr[0]==0);
-           }
-           
-           if(dp[ind][T]!=-1)
-               return dp[ind][T];
-               
-           int notTaken = f(arr,ind-1,T,dp);
-           
-           int taken = 0;
-           if(arr[ind] <= T)
-               taken = f(arr,ind,T-arr[ind],dp);
-               
-           return dp[ind][T] = taken + notTaken;
-    }
-
     int change(int amount, vector<int>& coins) {
        
         int n= coins.size();
     
-        vector<vector<int>> dp(n,vector<int>(amount+1, -1));
+        vector<vector<int>> dp(n, vector<int>(amount+1, 0));
         
-        return f(coins, n-1, amount, dp); 
+        for(int T=0; T<=amount; T++)
+        {
+            if(T % coins[0]==0)
+                dp[0][T] = 1;
+        }
+        
+        for(int idx=1; idx<n; idx++)
+        {
+            for(int tar = 0; tar <= amount; tar++)
+            {
+                int notTaken = dp[idx-1][tar];
+           
+                int taken = 0;
+                if(coins[idx] <= tar)
+                    taken = dp[idx][tar-coins[idx]];
+               
+                dp[idx][tar] = taken + notTaken;
+            }
+        }
+        
+        return dp[n-1][amount]; 
     }
 };
