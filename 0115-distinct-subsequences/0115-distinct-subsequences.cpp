94d928d0d090d273375d18181a2fe7b8593fe07c
Ayushi Chouhan <ayushichouhan2003@gmail.com>
2023-10-03 00:51:42 +0530

    Time: 2587 ms (5.01%), Space: 420.4 MB (12.31%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    int countUtil(string s1, string s2, int ind1, int ind2,vector<vector<int>>& dp){
+    if(ind2<0)
+        return 1;
+    if(ind1<0)
+        return 0;
+    
+    if(dp[ind1][ind2]!=-1)
+        return dp[ind1][ind2];
+    
+    if(s1[ind1]==s2[ind2]){
+        int leaveOne = countUtil(s1,s2,ind1-1,ind2-1,dp);
+        int stay = countUtil(s1,s2,ind1-1,ind2,dp);
+        
+        return dp[ind1][ind2] = (leaveOne + stay);
+    }
+    
+    else{
+        return dp[ind1][ind2] = countUtil(s1,s2,ind1-1,ind2,dp);
+    }
+}
+    
+    
+    int numDistinct(string s, string t) {
+        
+        int ls = s.length(), lt = t.length();
+        vector<vector<int>> dp(ls,vector<int>(lt,-1));
+        
+        return countUtil(s,t,ls-1,lt-1,dp);
+    }
+};
