f82a108a7c1c50ae5ffa2f5a23ea0bd98895d5bb
Ayushi Chouhan <ayushichouhan2003@gmail.com>
2023-10-13 01:14:37 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (15.47%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -2,13 +2,36 @@ class Solution {
 public:
     int mySqrt(int x) {
         
-        long ans;
-        for(long i=0; i<=x; i++)
+//         long ans;
+//         for(long i=0; i<=x; i++)
+//         {
+//             if(i*i <= x)
+//                 ans = i;
+//             else if(i*i > x)
+//                 break;
+//         }
+        
+//         return (int)ans;
+        
+        long ans = 0;
+        long l = 0, h = x;
+        while(l<=h)
         {
-            if(i*i <= x)
-                ans = i;
-            else if(i*i > x)
+           long mid = l + (h-l)/2;
+            if(mid*mid == x)
+            {
+                ans = mid;
                 break;
+            }
+            else if(mid*mid < x)
+            {
+                ans = mid;
+                l = mid+1;
+            }
+            else
+            {
+                h = mid-1;
+            }
         }
         
         return (int)ans;
