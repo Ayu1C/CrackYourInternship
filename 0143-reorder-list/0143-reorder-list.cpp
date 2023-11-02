c5ce47485aeec130964c7dc91fbe6598bb54fb0c
Ayushi Chouhan <ayushichouhan2003@gmail.com>
2023-11-03 01:31:00 +0530

    Time: 24 ms (77.25%), Space: 18.8 MB (28.33%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reorderList(ListNode* head){
+         
+        // if(head->next==NULL) return head;
+        // ListNode* cur = head->next;
+        // ListNode* temp = reverse(cur);
+        // cur->next = head; 
+        // return temp;
+        
+        if ((!head) || (!head->next) || (!head->next->next)) return head; // Edge cases
+        
+        stack<ListNode*> my_stack;
+        ListNode* ptr = head;
+        int size = 0;
+        while (ptr != NULL) // Put all nodes in stack
+        {
+            my_stack.push(ptr);
+            size++;
+            ptr = ptr->next;
+        }
+        
+        ListNode* pptr = head;
+        for (int j=0; j<size/2; j++) // Between every two nodes insert the one in the top of the stack
+        {
+            ListNode *element = my_stack.top();
+            my_stack.pop();
+            element->next = pptr->next;
+            pptr->next = element;
+            pptr = pptr->next->next;
+        }
+        pptr->next = NULL;
+        
+        return head;
+    }
+};
