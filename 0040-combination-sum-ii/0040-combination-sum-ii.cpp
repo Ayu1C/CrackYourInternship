b8c75cfef4c315c54648f2d9266cba919407c4a6
Ayushi Chouhan <ayushichouhan2003@gmail.com>
2023-11-10 03:16:04 +0530

    Time: 31 ms (9.74%), Space: 16.7 MB (11.98%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    void backtrack(vector<vector<int>>& res, vector<int> temp, vector<int>& nums, int remain, int start)
+    {
+       if(remain < 0) return;
+       else if(remain==0) res.push_back(temp);
+        else{
+            
+            for(int i=start; i<nums.size(); i++)
+            {
+                if(i>start && nums[i]==nums[i-1]) continue;
+                temp.push_back(nums[i]);
+                backtrack(res, temp, nums, remain-nums[i], i+1);
+                temp.pop_back();
+            }
+        }
+    }
+    
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+       
+        vector<vector<int>> res;
+        sort(candidates.begin(), candidates.end());
+       
+        backtrack(res, {}, candidates, target, 0);
+         
+        return res;
+    }
+};
