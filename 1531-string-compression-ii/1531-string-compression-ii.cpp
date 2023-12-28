982eea17c166d51cb4789d6f081654aca1ebe06b
Ayushi Chouhan <ayushichouhan2003@gmail.com>
2023-12-28 23:52:43 +0530

    Time: 216 ms (62.92%), Space: 16.1 MB (69.66%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.cpp
+++ 1531-string-compression-ii/1531-string-compression-ii.cpp
@@ -1,38 +1,37 @@
 class Solution {
 
-    int encodedLen(int len)
-    {
-        if (len <= 1) return len;
-        else if (len < 10) return 2;
-        else if (len < 100) return 3;
-        else return 4;
-    }
-
-    public:
+public:
     
-    int getLengthOfOptimalCompression(string s, int k)
-    {
+    int getLengthOfOptimalCompression(string s, int k) {
+        
         int n = s.length();
-        vector<vector<long>> dp(n+1,vector<long>(k+1,INT_MAX));
-        dp[0][0]=0;
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 0; j <= k; j++)
-            {
-                if (j > 0) 
-                    dp[i][j] = dp[i - 1][j - 1];
-                int removed = 0, count = 0;
-                for (int p = i; p > 0; p--)
-                {
-                    if (s[p-1] == s[i-1]) 
-                        count++;
-                    else if (++removed > j)
-                        break;
-                    dp[i][j] = min(dp[i][j], dp[p - 1][j - removed] + encodedLen(count));
+        vector<vector<int>> dp(110, vector<int>(110, 9999)); // Initializing a 2D vector 'dp' of size 110x110 with value 9999
+        dp[0][0] = 0; // Initializing the base case where no characters and deletions exist
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                int cnt = 0, del = 0;
+                for (int l = i; l >= 1; l--) {
+                    // Count the frequency of characters from 'i' to 'l'
+                    if (s[l - 1] == s[i - 1]) 
+                        cnt++;
+                    else 
+                        del++;
+                    
+                    // Check if the remaining allowed deletions are valid (j - del >= 0)
+                    if (j - del >= 0) {
+                        // Update the dp array based on the conditions
+                        dp[i][j] = min(dp[i][j],
+                            dp[l - 1][j - del] + 1 + (cnt >= 100 ? 3 : cnt >= 10 ? 2 : cnt >= 2 ? 1 : 0));
+                    }
                 }
+                
+                // If there are remaining allowed deletions (j > 0), consider the case without deleting current character
+                if (j > 0)
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j - 1]);
             }
         }
-        return dp[n][k];
+        return dp[n][k]; // Return the minimum length for 's' with at most 'k' deletions
     }
     
 };
