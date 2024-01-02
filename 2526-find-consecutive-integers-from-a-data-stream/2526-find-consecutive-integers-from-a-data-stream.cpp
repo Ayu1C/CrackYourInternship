4f6faf09b572c057cca7d5c7edc6932bad73bbaa
Ayushi Chouhan <ayushichouhan2003@gmail.com>
2024-01-03 00:55:02 +0530

    Time: 248 ms (71.05%), Space: 122.9 MB (70.30%) - LeetHub

--- 2526-find-consecutive-integers-from-a-data-stream/2526-find-consecutive-integers-from-a-data-stream.cpp
+++ 2526-find-consecutive-integers-from-a-data-stream/2526-find-consecutive-integers-from-a-data-stream.cpp
@@ -1,44 +1,20 @@
 class DataStream {
     
-    int dsValue, dsK;
-    map<int, int> mp;
-    queue<int> q;
-    
 public:
+    int val = 0, kmax = 0;
+    int valCount = 0;
     DataStream(int value, int k) {
-        
-        //declaring a map to store number of ele having value equal to "value"
-        dsValue = value;
-        dsK = k;
-        mp[dsValue] = 0;
+        val = value;
+        kmax = k;
     }
     
     bool consec(int num) {
-        
-        //push ele to queue, and update count in map if it is equal to value
-        q.push(num);
-        if(num == dsValue) mp[dsValue]++;
-        
-        bool ans = false;
-        
-        //if size of queue is less than k, return false
-        if(q.size() < dsK) return false;
-        
-        //if size of q == k, then we will check whether we have k num of value count in map or not, if yes, then return true, else return false.
-        
-        else if(q.size()==dsK)
-        {
-            if(mp[dsValue]==dsK) ans = true;
-            
-            //we will remove front ele from queue, so that on next call of consec, size of q again becomes equal to k, and we will reduce count of value, if removed element is equal to value.
-            
-            if(q.front()==dsValue) mp[dsValue]--;
-            q.pop();
-            
-        }
-        
-        return ans;
-    }    
+        if(num == val)
+            valCount++;
+        else
+            valCount = 0;
+        return valCount >= kmax;
+    }
 };
 
 /**
