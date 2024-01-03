3054fdb379717db182fcb4b206681661563397c1
Ayushi Chouhan <ayushichouhan2003@gmail.com>
2024-01-04 02:39:29 +0530

    Time: 38 ms (37.60%), Space: 7.1 MB (45.20%) - LeetHub

---
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,25 @@
+//f(n) = f(n-delay) + f(n-(delay+1)) +f(n-(delay+2)) ....f(n-(forget-1)) where n-i >=0. What this means is that each person tells the secret to forget-delay-1 people and they would do the same and so on.
+
+static int MOD=1e9+7;
+class Solution {
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        
+        vector<long> memo(n+1,1);    // Each day contributes at least to 1 person kowing the secret.
+        memo[0]=0;  // End case
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=delay;j<forget;j++)
+            {
+                // Number of people that the secret will be forwarded to
+                if(i-j>=0)
+                    memo[i]=(memo[i]+memo[i-j])%MOD;    // Same as our recursion relation.
+                //i-j means ki at ith day, ek person ko to share hua he, this is person A who knew already, hum usme add karenge ki aj se delay din pehle jin logo ko pta tha, wo aaj share kar sakte, aur uske pehle ke din ke bhi sab log jinne share kiya, lekin agr 4 din pehle koi bhul gya ho to hum 4 din pehle ka count nhi karenge Read recurrence relation at top for more info.
+             }
+        }
+            
+        
+        return (memo[n]-memo[n-forget]+MOD)%MOD;  // Subtract the people who found out by the `n-forget` day as observed.
+        
+    }
+};
