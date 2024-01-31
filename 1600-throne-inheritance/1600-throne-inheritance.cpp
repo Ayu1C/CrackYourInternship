f70c4262f2b1b9ae719bdb389779a0e162baab19
Ayushi Chouhan <ayushichouhan2003@gmail.com>
2024-01-31 20:57:05 +0530

    Time: 449 ms (40.50%), Space: 185.9 MB (15.70%) - LeetHub

---
+++ 1600-throne-inheritance/1600-throne-inheritance.cpp
@@ -0,0 +1,42 @@
+class ThroneInheritance {
+
+    unordered_map<string, bool> dead;   // Just to keep track of alive people
+    unordered_map<string, vector<string>> family;  
+    string root;
+    
+public:
+    ThroneInheritance(string kingName) {
+        root = kingName;
+    }
+    
+    void birth(string parentName, string childName) {
+        family[parentName].push_back(childName);    // Appending at the end takes care of oldest to yongest 
+    }
+    
+    void death(string name) {
+        dead[name] = true;
+    }
+    
+    void dfs(vector<string> &ans, string root) {
+        if (!dead[root]) {           // Just check if dead before inserting into the order.
+            ans.push_back(root);
+        }
+        for (string child: family[root]) {
+            dfs(ans, child);
+        }
+    }
+    
+    vector<string> getInheritanceOrder() {
+        vector<string> ans;
+        dfs(ans, root);
+        return ans;
+    }
+};
+
+/**
+ * Your ThroneInheritance object will be instantiated and called as such:
+ * ThroneInheritance* obj = new ThroneInheritance(kingName);
+ * obj->birth(parentName,childName);
+ * obj->death(name);
+ * vector<string> param_3 = obj->getInheritanceOrder();
+ */
